e55d4a8d8f49b0bddba7cc7d4401033cc343b0cc
Jessica <you@example.com>
2021-05-09 19:20:04 +0200

    calling the API

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
-export default App;
+import React, {Component} from 'react';
+    
+    class App extends Component {
+      state = {
+        contacts:[]
+      }
+      componentDidMount(){
+        fetch('http://jsonplaceholder.typicode.com/users')
+        .then(res => res.json())
+        .then((data)=>{
+          this.setState({contacts : data})
+        })
+        .catch(console.log)
+      }
+      render () {
+        return (
+          <div className="card">
+            <div className="card-body">
+              <h5 classname="card-title">Steve Jobs</h5>
+              <h6 className="card-subtitle mb-2 text-muted">steve@apple.com</h6>
+              <p className="card-text">Stay Hungry, Stay Foolish</p>
+            </div>
+          </div>
+        )
+      }
+    }
+    
+    export default App
